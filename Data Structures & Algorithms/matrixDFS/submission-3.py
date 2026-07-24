3b3ba8a1fa2c3d205d6899484e015518f388cb71
Pavle Šarenac <131699620+PavleSarenac@users.noreply.github.com>
2026-07-24 19:32:07 +0200

    Add: matrixDFS - submission-3

---
+++ Data Structures & Algorithms/matrixDFS/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countPaths(self, grid: List[List[int]]) -> int:
+        self.totalUniquePaths = 0
+
+        def dfs(row, column):
+            if row < 0 or row >= len(grid) or column < 0 or column >= len(grid[0]) or grid[row][column] != 0:
+                return
+            if row == len(grid) - 1 and column == len(grid[0]) - 1 and grid[row][column] == 0:
+                self.totalUniquePaths += 1
+                return
+            
+            originalValue = grid[row][column]
+            grid[row][column] = 2
+
+            dfs(row, column - 1)
+            dfs(row - 1, column)
+            dfs(row, column + 1)
+            dfs(row + 1, column)
+
+            grid[row][column] = originalValue
+
+        dfs(0, 0)
+
+        return self.totalUniquePaths
