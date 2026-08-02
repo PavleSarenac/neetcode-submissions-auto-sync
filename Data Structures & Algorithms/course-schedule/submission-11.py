1210eedb52aa2f72213d91e9839558abdc5a29e8
Pavle Šarenac <131699620+PavleSarenac@users.noreply.github.com>
2026-08-02 12:39:14 +0200

    Add: course-schedule - submission-11

---
+++ Data Structures & Algorithms/course-schedule/submission-11.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjacencyList = dict()
+
+        for targetCourse, prerequisite in prerequisites:
+            if targetCourse not in adjacencyList:
+                adjacencyList[targetCourse] = set()
+            adjacencyList[targetCourse].add(prerequisite)
+
+            transitivePrerequisites = deque()
+            transitivePrerequisites.append(adjacencyList[targetCourse])
+            visitedTargets = set()
+            visitedTargets.add(targetCourse)
+            while transitivePrerequisites:
+                currentTransitivePrerequisites = transitivePrerequisites.popleft()
+                if targetCourse in currentTransitivePrerequisites:
+                    return False
+                for currentTransitivePrerequisite in currentTransitivePrerequisites:
+                    if currentTransitivePrerequisite in adjacencyList and currentTransitivePrerequisite not in visitedTargets:
+                        transitivePrerequisites.append(adjacencyList[currentTransitivePrerequisite])
+                        visitedTargets.add(currentTransitivePrerequisite)
+        return True
